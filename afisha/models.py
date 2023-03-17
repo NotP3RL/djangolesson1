b1c6429f64d97e358aa268225e2c844ca286f3f3
NotP3RL <84793664+NotP3RL@users.noreply.github.com>
2023-03-17 18:44:36 +0800

    Add <ваше название> model

--- afisha/models.py
+++ afisha/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Place(models.Model):
+    title = models.CharField('Заголовок', max_length=200)
+    short_description = models.TextField('Короткое название')
+    long_description = models.TextField('Длинное описание', blank=True)
+    lng = models.FloatField('Долгота')
+    lat = models.FloatField('Широта')
+
+    class Meta:
+        verbose_name = 'Место'
+        verbose_name_plural = 'Места'
+
+    def str(self):
+        return f'{self.title}'

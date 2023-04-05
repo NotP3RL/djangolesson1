74d50ae45e8a1ad802df8523be76b3d92e1f53d0
NotP3RL <84793664+NotP3RL@users.noreply.github.com>
2023-04-05 18:48:27 +0800

    Move data from template to view

--- afisha/views.py
+++ afisha/views.py
@@ -1,9 +1,27 @@
 from django.http import HttpResponse
 from django.template import loader
+from .models import Place
 
 
 def show_index(request):
     template = loader.get_template('index.html')
-    context = {}
+    places = Place.objects.all()
+    context = {
+        "places": {"type": "FeatureCollection",
+                   "features": [
+                       {
+                           "type": "Feature",
+                           "geometry": {
+                               "type": "Point",
+                               "coordinates": [place.lng, place.lat]
+                           },
+                           "properties": {
+                               "title": place.title,
+                               "placeId": place.id,
+                               "detailsUrl": "./static/places/moscow_legends.json"
+                           }
+                       } for place in places
+                   ]}
+    }
     rendered_page = template.render(context, request)
     return HttpResponse(rendered_page)

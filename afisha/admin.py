24e03556ff4a35a89a1e94f1733e5fb1eb0a2ceb
NotP3RL <84793664+NotP3RL@users.noreply.github.com>
2023-04-26 18:45:43 +0800

    добавил сортировку изображений к инлайну

--- afisha/admin.py
+++ afisha/admin.py
@@ -1,22 +1,23 @@
+from adminsortable2.admin import SortableTabularInline, SortableAdminBase
 from django.contrib import admin
 from django.utils.html import format_html
 from .models import Place, Image
 
 
-class ImageInline(admin.TabularInline):
+class ImageTabularInline(SortableTabularInline):
     model = Image
-    fields = ('image', 'preview', 'ordinal_number')
+    fields = ('image', 'preview', 'ordinal_number', )
     extra = 1
-    readonly_fields = ['preview']
+    readonly_fields = ['preview', ]
 
     def preview(self, obj):
         return format_html('<img src="{url}" height="200px" />', url=obj.image.url)
 
 
 @admin.register(Place)
-class PlaceAdmin(admin.ModelAdmin):
+class PlaceAdmin(SortableAdminBase, admin.ModelAdmin):
     list_display = ['title', ]
-    inlines = [ImageInline, ]
+    inlines = [ImageTabularInline, ]
 
 
 @admin.register(Image)
--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'afisha.apps.AfishaConfig'
+    'afisha.apps.AfishaConfig',
+    'adminsortable2',
 ]
 
 STATIC_URL = 'static/'

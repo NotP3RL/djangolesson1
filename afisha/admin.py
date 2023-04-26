29dae7dcf97345d126dfd26cf7c794bd54c859d6
NotP3RL <84793664+NotP3RL@users.noreply.github.com>
2023-04-26 18:20:06 +0800

    добавил превью изображения в инлайн

--- afisha/admin.py
+++ afisha/admin.py
@@ -1,10 +1,17 @@
 from django.contrib import admin
+from django.utils.html import format_html
 from .models import Place, Image
+
 
 class ImageInline(admin.TabularInline):
     model = Image
-    fields = ('image', 'ordinal_number')
+    fields = ('image', 'preview', 'ordinal_number')
     extra = 1
+    readonly_fields = ['preview']
+
+    def preview(self, obj):
+        return format_html('<img src="{url}" height="200px" />', url=obj.image.url)
+
 
 @admin.register(Place)
 class PlaceAdmin(admin.ModelAdmin):

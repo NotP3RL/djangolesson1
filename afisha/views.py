d62f725da2d291ff62d784e76c14e81f58738d53
NotP3RL <84793664+NotP3RL@users.noreply.github.com>
2023-06-26 17:13:01 +0800

    удалил ненужные импорты

--- afisha/views.py
+++ afisha/views.py
@@ -1,6 +1,5 @@
-from django.http import HttpResponse, JsonResponse
+from django.http import JsonResponse
 from django.shortcuts import get_object_or_404, render
-from django.template import loader
 from django.urls import reverse
 
 from .models import Place

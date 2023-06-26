385c2025858c96031aa18677b5963d9a81441451
NotP3RL <84793664+NotP3RL@users.noreply.github.com>
2023-06-26 17:24:19 +0800

    убрал ненужные исключения

--- afisha/management/commands/load_place.py
+++ afisha/management/commands/load_place.py
@@ -12,11 +12,11 @@ class Command(BaseCommand):
         response.raise_for_status()
         response_content = response.json()
         place, created = Place.objects.get_or_create(
-            title=response_content.get('title', []),
-            short_description=response_content.get('description_short', []),
-            long_description=response_content.get('description_long', []),
-            lng=response_content['coordinates'].get('lng', []),
-            lat=response_content['coordinates'].get('lat', [])
+            title=response_content['title'],
+            short_description=response_content.get('description_short', ''),
+            long_description=response_content.get('description_long', ''),
+            lng=response_content['coordinates']['lng'],
+            lat=response_content['coordinates']['lat']
         )
         if not created:
             return
